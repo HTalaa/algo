f4c89f59204bec6d70525bfa5162d83414814a2a
Hamid Talaa <eng.hamed88@gmail.com>
2020-04-08 10:56:31 +0200

    change

---
+++ exo02.java
@@ -0,0 +1,13 @@
+package algo;
+import java.util.Scanner;
+public class exo02 {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter your first name");
+        String fName=sc.nextLine();
+        System.out.println("Enter your last name");
+        String lName=sc.nextLine();
+        System.out.println("Bienvenu "+ fName +" "+lName);
+
+    }
+}
