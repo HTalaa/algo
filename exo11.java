c3f8be33dad0e95518178654c4d68c128447b53c
Hamid Talaa <eng.hamed88@gmail.com>
2020-04-10 16:56:51 +0200

    change&

--- exo11.java
+++ exo11.java
@@ -5,7 +5,9 @@ import java.util.Scanner;
 public class exo11 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        int note=sc.nextInt();
+        System.out.print("Enterz la note: ");
+        float x=Float.parseFloat(sc.next());
+        int note=(int)x;
         switch (note){
             case 0:
             case 1:
@@ -18,7 +20,30 @@ public class exo11 {
             case 8:
             case 9:
             case 10:
-                System.out.println("");
+                System.out.println("I");
+                break;
+            case 11:
+            case 12:
+                System.out.println("S");
+                break;
+            case 13:
+            case 14:
+            case 15:
+                System.out.println("B");
+                break;
+            case 16:
+            case 17:
+            case 18:
+                System.out.println("TB");
+                break;
+            case 19:
+            case 20:
+                System.out.println("Excellent");
+                break;
+            default:
+                System.out.println("PAS Valid");
+                break;
+
 
 
 
