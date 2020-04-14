29f57e59f9e34f84917fa15c93a695b3e5d029bc
Hamid Talaa <eng.hamed88@gmail.com>
2020-04-14 11:28:56 +0200

    change&

---
+++ exo13.java
@@ -0,0 +1,12 @@
+package algo;
+
+public class exo13 {
+    public static void main(String[] args) {
+        int x=0;
+        while (x<11)
+        {
+            System.out.printf("%d * 2 = %d \n",x,x*2);
+            x++;
+        }
+    }
+}
