e81cd8f0a75bbead5bc946a160f33254853bfa34
Hamid Talaa <eng.hamed88@gmail.com>
2020-04-15 09:57:26 +0200

    change&

---
+++ exo17.java
@@ -0,0 +1,71 @@
+package algo;
+
+import java.util.Scanner;
+
+public class exo17 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+//        exo17 À l’aide d’une boucle Faire … TantQue , améliorez l’algorithme du distributeur
+//        de boissons pour qu’il demande au client s’il désire une autre boisson (Tant qu’il en a
+//                envie).
+//
+        // Choisissez votre boisson : 1 pour coca, 2 pour eau
+        // 1
+        // Voici votre coca
+        // Désirez-vous une autre boisson ?
+        // true
+        // Choisissez votre boisson : 1 pour coca, 2 pour eau
+        // 1
+        // Voici votre coca
+        // Désirez-vous une autre boisson ?
+        // false
+        // Merci et au revoir
+        int stockCoca = 2, stockEau = 0;
+        boolean encore=false;
+        do{
+
+            System.out.println("Choisissez une boisson, tapez :\n" +
+                    "        1 : coca\n" +
+                    "        2 : eau\n Mon choix :");
+
+            String choix = sc.next();
+
+            switch (choix)
+            {
+                case "1": // coca
+                    if(stockCoca > 0)
+                    {
+                        System.out.println("Voici votre coca");
+                        stockCoca = stockCoca - 1;
+                        // stockCoca--;
+                        // stockCoca -= 1;
+                    }
+                    else
+                    {
+                        System.out.println("Il n'y a plus de coca");
+                    }
+                    break;
+                case "2": // eau
+                    if(stockEau > 0)
+                    {
+                        System.out.println("Voici votre eau");
+                        stockEau = stockEau - 1;
+                        // stockCoca--;
+                        // stockCoca -= 1;
+                    }
+                    else
+                    {
+                        System.out.println("Il n'y a plus de eau");
+                    }
+                    break;
+                default:
+                    System.out.println("Le choix n'est pas valide");
+
+            }
+            System.out.println("Il reste "+stockCoca+" coca et "+stockEau+" eau dans le distributeur");
+            System.out.println("Désirez-vous une autre boisson ? ");
+            encore=sc.nextBoolean();
+        }while(encore);
+        System.out.println(" Merci et au revoir");
+    }
+}
--- exoIFSup02.java
+++ exoIFSup02.java
@@ -2,23 +2,35 @@ package algo;
 
 import java.util.Scanner;
 
-public class exoIFSup01 {
+public class exoIFSup02 {
     public static void main(String[] args) {
-        int h,m;
+        int h,m,s;
         Scanner sc=new Scanner(System.in);
-        // donner l'heure qu'il sera une minute plus tard
+
+
+
+        // ===============
+        // EXO 2
+        // donner l'heure qu'il sera une seconde plus tard
         // --------
         // Entrez l'heure :
+        // 1
         System.out.print("Entrez l'heure : ");
         h=sc.nextInt();
-        // 1
         // Entrez les minutes :
+        // 59
         System.out.print("Entrez les minutes : ");
         m=sc.nextInt();
+        // Entrez les secondes:
         // 59
-        // Dans 1 min, il sera : 2h00
+        System.out.print("Entrez les minutes : ");
+        s=sc.nextInt();
+        // Dans 1 sec, il sera : 2:00:00
         if(h >=0 && h<24 && m>=0 && m<=60){
-            m+=1;
+            s+=1;
+            if(s==60){
+
+            }
             if(m==60){
                 m=0;
                 h+=1;
@@ -28,7 +40,7 @@ public class exoIFSup01 {
             }
             System.out.printf("Dans 1 min, il sera : %dh%02d",h,m);
         }
-       else{
+        else{
             System.out.println("le temps n'est pas valid");
         }
         // =================
