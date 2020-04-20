56f02fa101187c6c68a4756e022af20f40009e5b
Hamid Talaa <eng.hamed88@gmail.com>
2020-04-20 13:27:13 +0200

    change&

--- exo26.java
+++ exo26.java
@@ -2,7 +2,24 @@ package algo;
 
 public class exo26 {
     public static void main(String[] args) {
+        int temp=0;
         int tab1[]={9,1,8,7,3,2};
+        for(int i=0;i<tab1.length;++i){
+            for (int j=i+1;j<tab1.length;++j){
+                if(tab1[i]>tab1[j]){
+                    temp=tab1[j];
+                    tab1[j]=tab1[i];
+                    tab1[i]=temp;
+                }
+            }
+
+
+        }
+
+        for(int t:tab1){
+            System.out.println(t);
+        }
+
 
     }
 }
