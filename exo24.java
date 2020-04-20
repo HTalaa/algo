dc3d38987c1f884fa0008adde6a6f798e60b02a3
Hamid Talaa <eng.hamed88@gmail.com>
2020-04-20 11:36:28 +0200

    change&

--- exo24.java
+++ exo24.java
@@ -6,16 +6,32 @@ public class exo24 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         double tab[];
+        try {
+            System.out.println("Entrez le nombre de joueurs entre 1 et 10");
+            int no=sc.nextInt();
+            if(no <0 && no >10){
+                System.out.println("le nombre n'est pas entre 1 et 10");
+            }
+            else{
+                tab=new double[no];
+                for(int i=0;i<no;++i){
+                    System.out.println("Entrez le score de joueur "+(i+1)+" :");
+                    tab[i]=Double.parseDouble(sc.next());
+                }
 
-        System.out.println("Entrez le nombre de joueurs entre 1 et 10");
-        int no=sc.nextInt();
+                double sum=0;
+                for(double d:tab){
+                    sum+=d;
+                }
+                System.out.println("le moyen est :"+(sum/no));
+            }
+        }catch (Exception e){
+            System.out.println("ce n'est pas un nombre");
+        }
 
-        if(no <0 && no >10){
-            System.out.println("le nombre n'est pas entre 1 et 10");
-        }
-        else{
-            tab=new double[no];
-        }
+
+
+
 
     }
 }
