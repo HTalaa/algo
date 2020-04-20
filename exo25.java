e18f3f6eafa5a4580f84a3575463e6f7eecfd240
Hamid Talaa <eng.hamed88@gmail.com>
2020-04-20 11:49:02 +0200

    change&

---
+++ exo25.java
@@ -0,0 +1,11 @@
+package algo;
+
+public class exo25 {
+    public static void main(String[] args) {
+        int tab1[]={5,6,7,8,9};
+        //int tab2[]=new int[tab1.length];
+        for(int i=tab1.length-1;i>=0;--i){
+            System.out.println(tab1[i]);
+        }
+    }
+}
