3c9335ffdacfffdb749c090bed03753cbcbbf659
Hamid Talaa <eng.hamed88@gmail.com>
2020-04-21 15:07:23 +0200

    change&

--- exo35.java
+++ exo35.java
@@ -1,44 +1,22 @@
 package algo;
 
+import java.util.Arrays;
+
 public class exo35 {
     public static void main(String[] args) {
         int tab1[]={9,2,3,6,7,1};
         int tab2[]={5,2,4,6,3,8};
-        int tab3[]=new int[12];
-        for (int t:tab1){
-            insert(tab3,t);
-            for (int t1:tab3){
-                System.out.print(t1+" | ");
+        merge(tab1,tab2);
 
-            }
-            System.out.println("");
-        }
-
-        for (int t:tab2){
-            insert(tab3,t);
-            for (int t1:tab3){
-                System.out.print(t1+" | ");
-
-            }
-            System.out.println("");
-        }
 
     }
-    public static void insert(int[] tab,int x){
-        for(int i=0;i<tab.length;++i){
-            if(x<tab[i]){
-                for (int j=tab.length-2;j>=i;--j){
-                    tab[j+1]=tab[j];
-                }
-                tab[i]=x;
-                break;
-            }
-            else if(tab[i]==0){
-                tab[i]=x;
-                break;
-            }
+    public static void merge(int[] tab1,int[] tab2){
+            int aLen=tab1.length;
+            int bLen=tab2.length;
+            int result[]=new int[aLen+bLen];
+            System.arraycopy(tab1,0,result,0,aLen);
+            System.arraycopy(tab2,0,result,aLen,bLen);
+        System.out.println(Arrays.toString(result));
 
-
-        }
     }
 }
